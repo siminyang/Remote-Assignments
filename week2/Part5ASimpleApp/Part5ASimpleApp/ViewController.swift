bf598dc8df1ffe15bbdf15cbb0a104a88916a43e
Nicky Yang <yasimi0222@gmail.com>
2024-06-28 22:47:38 +0800

    Add comments

--- week2/Part5ASimpleApp/Part5ASimpleApp/ViewController.swift
+++ week2/Part5ASimpleApp/Part5ASimpleApp/ViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // 將Label標籤名設為textChange
     @IBOutlet weak var textChange: UILabel!
     
+    // text prompt 7個選項
     let text = [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus.",
         "Contrary to popular belief, Lorem Ipsum is not simply random text.",
@@ -21,18 +23,20 @@ class ViewController: UIViewController {
         "The first line of Lorem Ipsum, Lorem ipsum dolor sit amet..",
     ]
 
+    // 背景七個顏色
     let backgroundcolors: [UIColor] = [
         .red, .orange, .yellow, .green, .blue, .black, .purple
         ]
     
+    // 初始化的畫面設定：紅色、Lorem
     override func viewDidLoad() {
         super.viewDidLoad()
         textChange.text = text[0]
         view.backgroundColor = backgroundcolors[0]
     }
 
+    // 定義 Button (Show Another Fun Fact) 的功能：隨機選擇text & 背景顏色
     @IBAction func changeBackgroundAndText(_ sender: UIButton) {
-        UIButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
         let randomTextIndex = Int.random(in: 0 ..< text.count)
         let randomColorIndex = Int.random(in: 0 ..< backgroundcolors.count)
         textChange.text = text[randomTextIndex]
